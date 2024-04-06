a9c3722f92cbdb8bf888c92f13bc60867fc1d289
Dejan <dejan.bojic.001@gmail.com>
2024-04-06 20:27:18 +0200

    Start Project

---
+++ Mobile_Store2/Mobile_Store2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mobile_Store2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
